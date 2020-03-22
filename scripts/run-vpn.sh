ba44c76e502e6e4d48fbbbcda9e50660c34570bb
Damian Troy <github@black.hole.com.au>
2020-03-23 07:07:20 +1100

    Add container rebuild flag

--- scripts/run-vpn.sh
+++ scripts/run-vpn.sh
@@ -3,7 +3,32 @@
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
 source ${BASEDIR}/env.sh
 
+while getopts "r" OPT; do
+    case "$OPT" in
+        r)  REBUILD=true
+    esac
+done
+
+if [[ "$REBUILD" == "true" ]]; then
+    echo "* Stopping and destroying Deluge"
+    sudo systemctl stop deluge-container.service
+    sudo podman stop deluge
+    sudo podman rm deluge
+
+    echo "* Stopping and destroying Jackett"
+    sudo systemctl stop jackett-container.service
+    sudo podman stop jackett
+    sudo podman rm jackett
+
+    echo "* Stopping and destroying VPN"
+    sudo systemctl stop vpn-container.service
+    sudo podman stop vpn
+    sudo podman rm vpn
+fi
+
+echo "* Starting VPN"
 sudo podman run -d --cap-add=NET_ADMIN --device /dev/net/tun \
+    --network host \
     --name=vpn \
     --security-opt="label=disable" \
     -e TZ=${TZ} \
@@ -13,3 +38,14 @@ sudo podman run -d --cap-add=NET_ADMIN --device /dev/net/tun \
     ${VPN_IMAGE}:${VPN_TAG} \
     -r ${LOCAL_NET_CIDR} \
     -f ""
+
+if [[ "$REBUILD" == "true" ]]; then
+    sudo systemctl start vpn-container.service
+
+    $BASEDIR/run-jackett.sh
+    sudo systemctl start jackett-container.service
+
+    $BASEDIR/run-deluge.sh
+    sudo systemctl start deluge-container.service
+fi
+
