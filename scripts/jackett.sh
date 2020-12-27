80acd44756cd6cf91b67896f8419cf60ba76f855
Damian Troy <github@black.hole.com.au>
2020-12-28 07:17:36 +1100

    Quote cleanup

--- scripts/jackett.sh
+++ scripts/jackett.sh
@@ -21,12 +21,12 @@ sudo podman run -d \
     --name=jackett \
     --network container:vpn \
     --security-opt="label=disable" \
-    -e PUID="${PUID}" \
-    -e PGID="${PGID}" \
-    -e TZ="${TZ}" \
-    -v "${JACKETT_CONFIG_DIR}:/config:Z" \
-    -v "${VIDEOS_DIR}:/videos:z" \
-    "${JACKETT_IMAGE}"
+    -e PUID="$PUID" \
+    -e PGID="$PGID" \
+    -e TZ="$TZ" \
+    -v "$JACKETT_CONFIG_DIR:/config:Z" \
+    -v "$VIDEOS_DIR:/videos:z" \
+    "$JACKETT_IMAGE"
 
 if [[ "$REBUILD" == "true" ]]; then
     sudo systemctl start jackett-container.service
