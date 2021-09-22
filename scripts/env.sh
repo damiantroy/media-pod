4fc809c6d48183b2c5c8be4b97b306f0756f5e88
Damian Troy <github@black.hole.com.au>
2021-09-23 07:04:26 +1000

    Now using Podman auto-updater

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,38 +1,39 @@
 # Personal
-TZ=Australia/Melbourne
-LOCAL_NET_CIDR=192.168.1.0/24
+export TZ=Australia/Melbourne
+export LOCAL_NET_CIDR=192.168.1.0/24
 
 # User and group
-PUID=1001
-PGID=1001
-APP_USER=videos
-APP_GROUP=videos
+export PUID=1001
+export PGID=1001
+export APP_USER=videos
+export APP_GROUP=videos
 
 # Image Tags
-PLEX_TAG=plexpass # Options: latest, public, plexpass
-VPN_TAG=latest
-NGINX_TAG=latest
+# PLEX_TAG Options: latest, public, plexpass
+export PLEX_TAG=plexpass
+export VPN_TAG=latest
+export NGINX_TAG=latest
 
 # Directories
-VIDEOS_DIR=/videos
-VPN_CONFIG_DIR=/etc/openvpn
-JACKETT_CONFIG_DIR=/etc/jackett
-SABNZBD_CONFIG_DIR=/etc/sabnzbd
-QBITTORRENT_CONFIG_DIR=/etc/qbittorrent
-SONARR_CONFIG_DIR=/etc/sonarr
-RADARR_CONFIG_DIR=/etc/radarr
-PLEX_CONFIG_DIR=/var/lib/plexmediaserver
-PLEX_TRANSCODE_DIR=/videos/plex
-NGINX_CONFIG_DIR=/etc/nginx
-NGINX_CACHE_DIR=/var/cache/nginx
-NGINX_RUN_DIR=/run/nginx
+export VIDEOS_DIR=/videos
+export VPN_CONFIG_DIR=/etc/openvpn
+export JACKETT_CONFIG_DIR=/etc/jackett
+export SABNZBD_CONFIG_DIR=/etc/sabnzbd
+export QBITTORRENT_CONFIG_DIR=/etc/qbittorrent
+export SONARR_CONFIG_DIR=/etc/sonarr
+export RADARR_CONFIG_DIR=/etc/radarr
+export PLEX_CONFIG_DIR=/var/lib/plexmediaserver
+export PLEX_TRANSCODE_DIR=/videos/plex
+export NGINX_CONFIG_DIR=/etc/nginx
+export NGINX_CACHE_DIR=/var/cache/nginx
+export NGINX_RUN_DIR=/run/nginx
 
 # Images
-VPN_IMAGE=dperson/openvpn-client
-JACKETT_IMAGE=damiantroy/jackett
-SABNZBD_IMAGE=damiantroy/sabnzbd
-QBITTORRENT_IMAGE=damiantroy/qbittorrent
-RADARR_IMAGE=damiantroy/radarr
-SONARR_IMAGE=damiantroy/sonarr
-PLEX_IMAGE=plexinc/pms-docker
-NGINX_IMAGE=library/nginx
+export VPN_IMAGE=docker.io/dperson/openvpn-client
+export JACKETT_IMAGE=docker.io/damiantroy/jackett
+export SABNZBD_IMAGE=docker.io/damiantroy/sabnzbd
+export QBITTORRENT_IMAGE=docker.io/damiantroy/qbittorrent
+export RADARR_IMAGE=docker.io/damiantroy/radarr
+export SONARR_IMAGE=docker.io/damiantroy/sonarr
+export PLEX_IMAGE=docker.io/plexinc/pms-docker
+export NGINX_IMAGE=docker.io/library/nginx
